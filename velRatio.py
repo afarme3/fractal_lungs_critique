36cb1f68a73a3c9e31ab588c462b334a6601c50e
afarme3 <64613596+afarme3@users.noreply.github.com>
2020-12-07 10:10:36 -0500

    Updated with better comments.

--- velRatio.py
+++ velRatio.py
@@ -8,17 +8,17 @@ finalVelocity = 0
 
 def duct(d, v, i, t): #Diameter, velocity, index, and (global) tree exponent are inputs. 
     global bifurcationLimit
-    newD = d/2.0
-    newV = pow(2,((-t+2)/t))*v
+    newD = pow(2,(-1/t))*d
+    newV = pow(2,(-(t-2)/t))*v
 
-    if i < bifurcationLimit:
+    if i < bifurcationLimit: #Checks if the tree has hit generation 14. Records index, velocity, and diameter for each generation. 
         if i not in bifurcationInfo[0]:
             bifurcationInfo[0].append(i)
             bifurcationInfo[1].append(newD)
             bifurcationInfo[2].append(newV)
         i += 1
         duct(newD, newV, i, t)
-    else:
+    else: #Exit condition for function loop. Once 14 generations reached, final diameter and velocity are exported. 
         global finalDiameter
         global finalVelocity
         finalDiameter = d
@@ -30,7 +30,7 @@ initVelocity = 51 #in cm/s. Also provided by the paper.
 multiDucts = [[2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0],[]] 
 #Stores final vfinal/vinit ratio for each value of t
 
-for treeExponent in multiDucts[0]:
+for treeExponent in multiDucts[0]: #Simulates a bronchial tree for each tree exponent, and records the velocity ratio.
     duct(initDiameter,initVelocity,index,treeExponent)
     multiDucts[1].append((finalVelocity/initVelocity))
 
@@ -38,7 +38,7 @@ print(multiDucts)
 
 
 
-plt.plot(multiDucts[0], multiDucts[1])
+plt.plot(multiDucts[0], multiDucts[1]) #Matplotlib plot information. 
 plt.title("Final Reduction in Flow Velocity versus Fractal Dimension of Bronchial Tree")
 plt.grid(b=True, which='both', axis='both')
 plt.ylabel("Final Velocity/Initial Velocity")
