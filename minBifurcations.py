a45f6cf1ae4a3e8713892aa97a73fe63e6d136dd
afarme3 <64613596+afarme3@users.noreply.github.com>
2020-12-07 10:44:06 -0500

    Added final diameter subplot

--- minBifurcations.py
+++ minBifurcations.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 
 finalIndex = 0
 index = 0
+finalDiameter = 0
 initDiameter = 1.8 #in centimeters. Same trachea diameter used in the paper.
 initVelocity = 51 #in cm/s. Also provided by the paper.
 velRatio = 0
 
-ductInfo = [[2.05,2.1,2.15,2.2,2.25,2.3,2.35,2.4,2.45,2.5,2.55,2.6,2.65,2.7,2.75,2.8,2.85,2.9,2.95,3.0],[],[]]
-#Stores data on each fractal landscape:Tree diameter exponent, final # of bifurcations, and final/init velocity ratio. 
+ductInfo = [[2.05,2.1,2.15,2.2,2.25,2.3,2.35,2.4,2.45,2.5,2.55,2.6,2.65,2.7,2.75,2.8,2.85,2.9,2.95,3.0],[],[],[]]
+#Stores data on each fractal landscape:Tree diameter exponent, final # of bifurcations, final/init velocity ratio, and diameter of final generation. 
 
 
 def duct(d, v, i, t): #Diameter, velocity, index, and (global) tree exponent are inputs. 
@@ -22,31 +23,43 @@ def duct(d, v, i, t): #Diameter, velocity, index, and (global) tree exponent are
         newV = pow(2,(-(t-2)/t))*v
         i += 1
         duct(newD,newV,i,t)
-    else: #Exit condition for the function loop, when the peclet number is <= 1. 
-        global finalIndex 
+    else:
+        global finalIndex
+        global finalDiameter
         global velRatio
         finalIndex = i+1 #Records the final number of bifurcations needed after tree completes. Adds 1 to include generation 0. 
         velRatio = v/initVelocity
+        finalDiameter = d
 
 
 
-for treeExponent in ductInfo[0]: #Iterates through input tree exponents and records the final # of bifurcations and init/final velocity ratio produced by each tree. 
+for treeExponent in ductInfo[0]:
     duct(initDiameter,initVelocity,index,treeExponent)
     ductInfo[1].append(finalIndex)
     ductInfo[2].append(velRatio)
+    ductInfo[3].append(finalDiameter)
 
 print(ductInfo)
 
-plt.subplot(1,2,1)
+plt.subplot(2,2,1)
 plt.plot(ductInfo[0], ductInfo[1])
 plt.grid(b=True, which='both', axis='both')
 plt.title("Number of Bifurcations Needed for Optimal Diffusion Across Fractal Dimensions")
 plt.ylabel("Minimum # of bifurcations for optimal diffusion")
 plt.xlabel("Tree Diameter Exponent (fractal dimension)")
 
-plt.subplot(1,2,2)
-plt.plot(ductInfo[0], ductInfo[2])
-plt.title("Final/Initial velocity ratio for various fractal dimensions")
+plt.subplot(2,2,2)
+plt.plot(ductInfo[0], ductInfo[2],)
+plt.grid(b=True, which='both', axis='both')
+plt.title("Final Reduction in Flow Velocity versus Fractal Dimension, with varied # of Bronchial Generations ")
 plt.ylabel("Final Velocity / Initial Velocity")
 plt.xlabel("Tree Diameter Exponent (fractal Dimension)")
+plt.show()
+
+plt.subplot(2,2,4)
+plt.plot(ductInfo[3], ductInfo[3])
+plt.grid(b=True, which='both', axis='both')
+plt.title("Final Diameter of Bronchial Tube across Fractal Dimensions")
+plt.xlabel("Tree Diameter Exponent (fractal dimension)")
+plt.ylabel("Diameter of Final Tube Generation, cm")
 plt.show()
